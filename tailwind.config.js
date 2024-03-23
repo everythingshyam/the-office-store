b3447eb578f08c972c781eeeb827a08d0099cdc7
cogito-shyam <stiwari@cogitocorp.com>
2024-03-24 00:48:28 +0530

    worked on header, footer and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,30 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				accent: '#007BFF',
-				secondary: '#111213',
-				primary: '#F8F8F8',
+				accent: {
+					2: '#B2E5F8',
+					4: '#77C2FA',
+					6: '#3B9EFD',
+					8: '#007BFF',
+				},
+				secondary: {
+					2: '#9FA8B5',
+					4: '#70767F',
+					6: '#404449',
+					8: '#111213',
+				},
+				primary: {
+					2: '#CAD1DA',
+					4: '#D9DEE4',
+					6: '#E9EBEE',
+					8: '#F8F8F8',
+				},
+			},
+			height: {
+				'50vh': '50dvh',
+			},
+			minHeight: {
+				'10vh': '15vh',
 			},
 		},
 	},
